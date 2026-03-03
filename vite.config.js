085d0ab6227c6a74dcac8ac6ec5892c55f8ffe7b
Eilith404 <minaya.sofia@gmail.com>
2026-03-03 12:04:48 +0100

    Update scripts

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // Cambia 'usuario' y 'repositorio' por los correctos
+  base: '/sofia-minaya-portfolio/',
   server: {
     open: '/',
     watch: {
